94a5bb66d53b5ac1144e289f1da5e5638ec2d7c4
Cucazer <cucazik@inbox.lv>
2021-03-07 16:50:43 +0100

    Fixed plot formatting as from SO

--- main.py
+++ main.py
@@ -81,6 +81,17 @@ if __name__ == "__main__":
 
             import matplotlib.pyplot as plt
             import numpy as np
+            from matplotlib.ticker import FuncFormatter
+            import matplotlib.ticker as ticker
+
+            def format_func(x, pos):
+                hours = int(x//3600)
+                minutes = int((x%3600)//60)
+                seconds = int(x%60)
+
+                return "{:d}:{:02d}".format(hours, minutes)
+                # return "{:d}:{:02d}:{:02d}".format(hours, minutes, seconds)
+
 
             plt.rcdefaults()
             fig, ax = plt.subplots()
@@ -89,13 +100,17 @@ if __name__ == "__main__":
             y_pos = np.arange(len(tasks))
             performance = [sum([y.get_duration() for y in x.time_entries], datetime.timedelta()) for x in tasks]
 
+            formatter = FuncFormatter(format_func)
             ax.barh(y_pos, [x.seconds for x in performance], align='center', color='green')
-            #ax.yaxis_date()
-            #ax.yaxis.set_major_formatter(mdates.DateFormatter("%H:%M"))
+            ax.xaxis.set_major_formatter(formatter)
+            # this locates y-ticks at the hours
+            #ax.xaxis.set_major_locator(ticker.MultipleLocator(base=3600))
+            # this ensures each bar has a 'date' label
+            #ax.yaxis.set_major_locator(ticker.MultipleLocator(base=1))
             ax.set_yticks(y_pos)
             ax.set_yticklabels([x.name for x in tasks])
             ax.invert_yaxis()  # labels read top-to-bottom
-            ax.set_xlabel('Time (s)')
+            ax.set_xlabel('Time')
             ax.set_title('How much time did you spend on tasks?')
 
             plt.show()
