6fa5cfe2147400e2957a5fedf6133d199f79e9a1
Cucazer <cucazik@inbox.lv>
2021-03-30 12:28:05 +0200

    Added choice wheel functionality

--- main_form.py
+++ main_form.py
@@ -1,6 +1,7 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.clock import Clock
+from kivy.properties import ObjectProperty
 
 import datetime
 
@@ -23,7 +24,14 @@ except:
 
 
 class ChoiceWheel(Widget):
+    category = ObjectProperty()
+    task = ObjectProperty()
+
     class Mode(Enum):
+        Switch = auto()
+        Create = auto()
+
+    class ItemType(Enum):
         Categories = auto()
         Tasks = auto()
 
@@ -33,36 +41,71 @@ class ChoiceWheel(Widget):
     
     def init_ui(self, dt=0):
         self.choice_buttons = [self.ids.button0, self.ids.button1, self.ids.button2, self.ids.button3, self.ids.button4]
-        self.mode = self.Mode.Categories
+        self.mode = self.Mode.Switch
+        self.items = self.ItemType.Categories
         self.update_choice_buttons(session.query(m.Category).all())
 
-    def update_choice_buttons(self, names):
+    def update_choice_buttons(self, names, disable = False):
         for i in range(min(len(names), len(self.choice_buttons))):
             self.choice_buttons[i].text = names[i].name
-            self.choice_buttons[i].disabled = False
+            self.choice_buttons[i].disabled = disable
         for i in range(len(names), len(self.choice_buttons)):
             self.choice_buttons[i].text = ""
             self.choice_buttons[i].disabled = True
         if len(names) < len(self.choice_buttons):
             self.ids.more.disabled = True
 
-    def on_button_click(self, button):
-        if self.mode == self.Mode.Categories:
-            category = session.query(m.Category).filter(m.Category.name == button.text).first()
-            self.update_choice_buttons(category.tasks)
-            self.mode = self.Mode.Tasks
+    def update_task_edit(self, text = None):
+        task_name_edit = self.ids.task_name
+        if text:
+            task_name_edit.disabled = False
+            task_name_edit.focus = True
+            task_name_edit.text = text
+            Clock.schedule_once(lambda dt: task_name_edit.select_all())
+        else:
+            task_name_edit.disabled = True
+            task_name_edit.text = ""
 
-            # task_name_edit = self.ids.task_name
-            # task_name_edit.disabled = False
-            # task_name_edit.focus = True
-            # task_name_edit.text = "New task..."
-            # task_name_edit.select_all()
-        elif self.mode == self.Mode.Tasks:
-            pass
+    def on_button_click(self, button):
+        if self.items == self.ItemType.Categories:
+            self.category = session.query(m.Category).filter(m.Category.name == button.text).first()
+            self.items = self.ItemType.Tasks
+            self.update_choice_buttons(self.category.tasks, self.mode == self.Mode.Create)
+
+            if self.mode == self.Mode.Create:
+                self.update_task_edit("New task...")
+        elif self.items == self.ItemType.Tasks:
+            self.task = next(x for x in self.category.tasks if x.name == button.text)
+            self.update_choice_buttons(session.query(m.Category).all())
+            self.items = self.ItemType.Categories
+            self.mode = self.Mode.Switch
+            self.update_task_edit()
+
+            print(f"Category {self.category.name} and task {self.task.name} chosen!")
         else:
             pass
 
-    def on_more_button_click(self, button):
+    def on_input_submit(self, input):
+        if self.items == self.ItemType.Categories:
+            self.category = m.Category(name=input.text)
+            self.update_choice_buttons(self.category.tasks, self.mode == self.Mode.Create)
+            self.items = self.ItemType.Tasks
+
+            if self.mode == self.Mode.Create:
+                self.update_task_edit("New task...")
+        elif self.items == self.ItemType.Tasks:
+            self.task = m.Task(name=input.text)
+            #self.category.tasks.append(self.task)
+            self.update_choice_buttons(session.query(m.Category).all())
+            self.items = self.ItemType.Categories
+            self.mode = self.Mode.Switch
+            self.update_task_edit()
+
+            print(f"Category {self.category.name} and task {self.task.name} chosen!")
+        else:
+            pass
+
+    def on_more_button_click(self):
         pass
 
 class MainForm(Widget):
@@ -81,7 +124,8 @@ class MainForm(Widget):
         task_name_edit.select_all()
 
         cw = self.ids.choice_wheel
-        cw.mode = cw.Mode.Categories
+        cw.mode = cw.Mode.Create
+        cw.items = cw.ItemType.Categories
         cw.update_choice_buttons(session.query(m.Category).all())
 
     def generate_plot(self):
