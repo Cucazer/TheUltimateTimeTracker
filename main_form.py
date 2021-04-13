4d484d2e390b3df42b46e142983abf1be0789da4
Cucazer <cucazik@inbox.lv>
2021-04-13 02:38:09 +0200

    Added ability to manually enter task times

--- main_form.py
+++ main_form.py
@@ -5,6 +5,8 @@ from kivy.properties import ObjectProperty
 
 from kivymd.app import MDApp
 #from kivymd.uix.datatables import MDDataTable
+from kivymd.uix.picker import MDDatePicker, MDTimePicker
+from kivymd.uix.snackbar import Snackbar
 
 import datetime
 
@@ -33,6 +35,7 @@ class ChoiceWheel(Widget):
     class Mode(Enum):
         Switch = auto()
         Create = auto()
+        Select = auto()
 
     class ItemType(Enum):
         Categories = auto()
@@ -134,6 +137,12 @@ class ChoiceWheel(Widget):
 class MainForm(Widget):
     def __init__(self, *args, **kwargs):
         super(MainForm, self).__init__(*args, **kwargs)
+
+        self.start_date = None
+        self.start_time = None
+        self.end_date = None
+        self.end_time = None
+
         Clock.schedule_once(self.init_ui, 0)
     
     def init_ui(self, dt=0):
@@ -152,9 +161,9 @@ class MainForm(Widget):
         cw.items = cw.ItemType.Categories
         cw.update_choice_buttons(session.query(m.Category).all())
 
-    def on_task_changed(self, instance, value):
+    def on_task_changed(self, instance, task):
         if self.ids.choice_wheel.mode == self.ids.choice_wheel.Mode.Switch:
-            active_task = value
+            active_task = task
             self.ids.current_task.text = f"Current task: {str(active_task)}"
 
             # create time entry
@@ -163,8 +172,17 @@ class MainForm(Widget):
                 unfinished_time_entry.end_time = datetime.datetime.now()
             except:
                 pass
-            session.add(m.TimeEntry(task = value, start_time = datetime.datetime.now()))   
-            session.commit()  
+            session.add(m.TimeEntry(task = task, start_time = datetime.datetime.now()))   
+            session.commit()
+        elif self.ids.choice_wheel.mode == self.ids.choice_wheel.Mode.Select:
+            start_time = datetime.datetime.combine(self.start_date, self.start_time)
+            end_time = datetime.datetime.combine(self.end_date, self.end_time)
+            session.add(m.TimeEntry(task = task, start_time = start_time, end_time = end_time))   
+            session.commit()
+            #TODO increase duration?
+            Snackbar(text=f"Activity {task.name} ({task.category.name}) from {start_time} to {end_time} successfully recorded!").show() # change to open() after update
+            for widget in [self.ids.start_time, self.ids.end_time]:
+                widget.disabled = False
 
     def generate_plot(self):
         tasks = session.query(m.Task).all()
@@ -172,6 +190,43 @@ class MainForm(Widget):
         #TODO separate thread?
         import plotter
         plotter.generate_basic_plot(tasks)
+
+    def get_date(self, date):
+        self.grabbed_text_edit.text = str(date)
+        if self.grabbed_text_edit == self.ids.start_time:
+            self.start_date = date
+        else:
+            self.end_date = date
+        time_dialog = MDTimePicker()
+        time_dialog.bind(time=self.get_time)
+        time_dialog.open()
+
+    def get_time(self, instance, time):
+        self.grabbed_text_edit.text += " " + str(time)
+        if self.grabbed_text_edit == self.ids.start_time:
+            self.start_time = time
+        else:
+            self.end_time = time
+        self.grabbed_text_edit = None
+        if all(x is not None for x in [self.start_date, self.start_time, self.end_date, self.end_time]):
+            self.ids.select_task.disabled = False
+
+    def show_date_picker(self, touch):
+        if touch.grab_current not in [self.ids.start_time, self.ids.end_time]:
+            return
+        self.grabbed_text_edit = touch.grab_current
+        date_dialog = MDDatePicker(callback=self.get_date, max_date=datetime.date.today())
+        date_dialog.open()
+        touch.ungrab(self.grabbed_text_edit)
+        return True
+
+    def select_task(self):
+        cw = self.ids.choice_wheel
+        cw.mode = cw.Mode.Select
+        cw.items = cw.ItemType.Categories
+        cw.update_choice_buttons(session.query(m.Category).all())
+        for widget in [self.ids.start_time, self.ids.end_time, self.ids.select_task]:
+            widget.disabled = True
 
 class TheUltimateTimeTrackerApp(MDApp):
     def build(self):
